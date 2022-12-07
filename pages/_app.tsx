bc6fb8c3198d82b3943df9afe4f03e45dad3eb64
stevenbenmoha <steven.benmoha@gmail.com>
2022-12-06 21:31:00 -0500

    first

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/GitHubLink";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Mint a Descendant</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Mint a Descendant"
+        />
+        <meta
+          name="keywords"
+          content=""
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
